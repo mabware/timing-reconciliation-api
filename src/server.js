22ff7695f5dc7d78db4f8c925283ed8653087d10
Michael Bartlett <michaelabartlett@googlemail.com>
2021-03-13 22:13:14 +0000

    Add endpoint to retrieve current state

--- src/server.js
+++ src/server.js
@@ -15,6 +15,15 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 let drivers = [];
 
+app.get('/currentState', (req, res) => {
+  const currentState = {
+    staged: [drivers.filter(driver => driver.is('staged')).map(driver => driver.id)],
+    running: [drivers.filter(driver => driver.is('running')).map(driver => driver.id)],
+    finished: [drivers.filter(driver => driver.is('stagfinisheded')).map(driver => driver.id)],
+  }
+  res.json(currentState);
+});
+
 app.post('/stage', (req, res) => {
   drivers.push(new driverState(req.body.id));
   console.log(drivers);
@@ -23,13 +32,11 @@ app.post('/stage', (req, res) => {
 
 app.post('/start', (req, res) => {
   drivers.find(driver => driver.is('staged')).start(req.body.startTime);
-  console.log(drivers);
   res.sendStatus(204);
 });
 
 app.post('/finish', (req, res) => {
   drivers.find(driver => driver.is('running')).finish(req.body.finishTime);
-  console.log(drivers);
   res.sendStatus(204);
 });
 
@@ -37,7 +44,6 @@ app.post('/confirm', (req, res) => {
   const index = drivers.findIndex(driver => driver.id === req.body.id)
   const completedDriver = drivers.splice(index, 1);
   completedDriver[0].confirm(req.body.penalty || 0, req.body.wrongTest || false);
-  console.log(drivers);
   res.json({
     totalTime: completedDriver[0].wrongTest ? null : (completedDriver[0].finishTime - completedDriver[0].startTime)/1000,
     penalty: completedDriver[0].penalty
@@ -46,7 +52,6 @@ app.post('/confirm', (req, res) => {
 
 app.post('/reset', (req, res) => {
   drivers = []
-  console.log(drivers);
   res.sendStatus(204)
 });
 
