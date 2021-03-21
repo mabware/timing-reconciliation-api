413a45b3441dbaba769e8dc56c7366aea28be83f
mabware <michaelabartlett@googlemail.com>
2021-03-21 11:11:06 +0000

    cancel finish time

--- src/driverState.js
+++ src/driverState.js
@@ -1,37 +1,40 @@
-var StateMachine = require('javascript-state-machine');
+const StateMachine = require('javascript-state-machine');
 
-var driverState = StateMachine.factory({
+const DriverState = StateMachine.factory({
   init: 'staged',
   transitions: [
     { name: 'start', from: 'staged', to: 'running' },
     { name: 'finish', from: 'running', to: 'finished' },
     { name: 'confirm', from: 'finished', to: 'confirmed' },
+    { name: 'cancelFinish', from: 'finished', to: 'running' },
   ],
-  data: function(id) {
+  data(id) {
     return {
       id,
       startTime: 0,
       finishTime: 0,
       wrongTest: false,
       penalty: 0,
-    }
+    };
   },
   methods: {
-    onStart: function(lifecycle, startTime) {
+    onStart(lifecycle, startTime) {
       this.startTime = startTime;
     },
-    onFinish: function(lifecycle, finishTime) {
+    onFinish(lifecycle, finishTime) {
       this.finishTime = finishTime;
     },
-    onConfirm: function(lifecycle, penalty, wrongTest) {
+    onCancelFinish() {
+      this.finishTime = 0;
+    },
+    onConfirm(lifecycle, penalty, wrongTest) {
       this.penalty = penalty;
       this.wrongTest = wrongTest;
-      console.log(this.describe())
     },
-    describe: function() {
-      return `driver: ${this.id}, totalTime: ${(this.finishTime - this.startTime)/1000}, penalty: ${this.penalty}`;
-    }
-}
+    describe() {
+      return `driver: ${this.id}, totalTime: ${(this.finishTime - this.startTime) / 1000}, penalty: ${this.penalty}`;
+    },
+  },
 });
 
-module.exports = driverState;
+module.exports = DriverState;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
+const http = require('http');
+const io = require('socket.io');
 
-const swaggerDocument = require('./swagger.json')
-const driverState = require('./driverState');
+const swaggerDocument = require('./swagger.json');
+const DriverState = require('./driverState');
 
 const app = express();
 const port = process.env.PORT || 5001;
-var http = require( "http" ).createServer( app );
-var io = require('socket.io')(http,  {
+const httpServer = http.createServer(app);
+const ioSocket = io(httpServer, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
 app.use(express.json());
@@ -24,73 +26,91 @@ let drivers = [];
 
 app.get('/currentState', (req, res) => {
   const currentState = {
-    staged: drivers.filter(driver => driver.is('staged')).map(driver => driver.id),
-    running: drivers.filter(driver => driver.is('running')).map(driver => driver.id),
-    finished: drivers.filter(driver => driver.is('finished')).map(driver => driver.id),
-  }
+    staged: drivers.filter((driver) => driver.is('staged')).map((driver) => driver.id),
+    running: drivers.filter((driver) => driver.is('running')).map((driver) => driver.id),
+    finished: drivers.filter((driver) => driver.is('finished')).map((driver) => driver.id),
+  };
   res.json(currentState);
 });
 
 app.post('/stage', (req, res) => {
-  drivers.push(new driverState(req.body.id));
-  io.emit('stateUpdate');
+  drivers.push(new DriverState(req.body.id));
+  ioSocket.emit('stateUpdate');
   res.sendStatus(204);
 });
 
 app.get('/stage', (req, res) => {
-  res.json(drivers.filter(driver => driver.is('staged')).map(driver => driver.id));
+  res.json(drivers.filter((driver) => driver.is('staged')).map((driver) => driver.id));
 });
 
 app.post('/start', (req, res) => {
-  drivers.find(driver => driver.is('staged')).start(req.body.startTime);
-  io.emit('stateUpdate');
+  drivers.find((driver) => driver.is('staged')).start(req.body.startTime);
+  ioSocket.emit('stateUpdate');
   res.sendStatus(204);
 });
 
 app.get('/start', (req, res) => {
-  res.json(drivers.filter(driver => driver.is('running')).map(driver => driver.id));
+  res.json(drivers.filter((driver) => driver.is('running')).map((driver) => driver.id));
 });
 
 app.post('/finish', (req, res) => {
-  drivers.find(driver => driver.is('running')).finish(req.body.finishTime);
-  io.emit('stateUpdate');
+  drivers.find((driver) => driver.is('running')).finish(req.body.finishTime);
+  ioSocket.emit('stateUpdate');
+  res.sendStatus(204);
+});
+
+app.post('/cancelFinish', (req, res) => {
+  let finishedTime;
+  drivers.some((driver) => {
+    if (driver.is('finised')) {
+      const lastfinshedTime = finishedTime;
+      finishedTime = driver.finishTime;
+      driver.cancelFinish();
+      if (lastfinshedTime) {
+        driver.finish(lastfinshedTime);
+      }
+    }
+    return driver.id === req.body.id;
+  });
+  ioSocket.emit('stateUpdate');
   res.sendStatus(204);
 });
 
 app.get('/finish', (req, res) => {
-  res.json(drivers.filter(driver => driver.is('finished')).map(driver => driver.id));
+  res.json(drivers.filter((driver) => driver.is('finished')).map((driver) => driver.id));
 });
 
 app.post('/confirm', (req, res) => {
-  const index = drivers.findIndex(driver => driver.id === req.body.id)
+  const index = drivers.findIndex((driver) => driver.id === req.body.id);
   const completedDriver = drivers.splice(index, 1);
   completedDriver[0].confirm(req.body.penalty || 0, req.body.wrongTest || false);
-  io.emit('stateUpdate');
+  ioSocket.emit('stateUpdate');
   res.json({
-    totalTime: completedDriver[0].wrongTest ? null : (completedDriver[0].finishTime - completedDriver[0].startTime)/1000,
-    penalty: completedDriver[0].penalty
+    totalTime:
+      completedDriver[0].wrongTest
+        ? null : (completedDriver[0].finishTime - completedDriver[0].startTime) / 1000,
+    penalty: completedDriver[0].penalty,
   });
 });
 
 app.delete('/driver/:id', (req, res) => {
-  const index = drivers.findIndex(driver => driver.id === req.params.id)
-  if (index != -1) {
+  const index = drivers.findIndex((driver) => driver.id === req.params.id);
+  if (index !== -1) {
     drivers.splice(index, 1);
-    io.emit('stateUpdate');
+    ioSocket.emit('stateUpdate');
     res.sendStatus(204);
   } else {
     res.sendStatus(404);
   }
-  
-})
-
-app.post('/reset', (req, res) => {
-  drivers = []
-  io.emit('stateUpdate');
-  res.sendStatus(204)
 });
 
-http.listen(port, () => {
+app.post('/reset', (req, res) => {
+  drivers = [];
+  ioSocket.emit('stateUpdate');
+  res.sendStatus(204);
+});
+
+httpServer.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`server is listening on ${port}`);
 });
